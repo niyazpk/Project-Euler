fa07e357e6d406385fc037a2ec1449524acf9b5e
Niyaz Puzhikkunnath <niyazpk@gmail.com>
2013-12-30 08:41:11 +0530

    Anwer was wrong in the progression method!

--- 1.py
+++ 1.py
@@ -5,6 +5,7 @@
 #-------------------------------------------
 
 
+
 # Simple solution
 sum = 0
 for i in range(1, 1000):
@@ -17,13 +18,14 @@ print (sum)
 # Solution using arithmetic progression equations
 sum = 0
 
-
-for i in (3,5):
-    n = int(999/i)
-    sum += n/2 * (2 * i + (n-1) * i)
+for i in (3, 5):
+    n = int(1000 / i)
+    x = n * (n + 1) * i / 2
+    sum += x
 
 i = 15
-n = int(999/i)
-sum -= n/2 * (2 * i + (n-1) * i) #This was added twice in the for loop.
+n = int(999 / i)
+x = n * (n + 1) * i / 2
+sum -= x  #This was added twice in the for loop.
 
-print(int(sum))
+print(sum)
